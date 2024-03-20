a4c362b95cfc3ebc35559df4d8c133bdd0936a47
Kirimino <brianonduso08@gmail.com>
2024-03-20 04:45:49 +0300

    Complete String Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Brian Onduso"
+
+const welcomeMessage = "Welcome to Flatbook, Brian Onduso !"
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, BRIAN ONDUSO !"
+
+const shortGreeting = "Welcome, Brian !"
